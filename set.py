c4f8d00c75dda6bfd8e721b9342a1b4e1b90c1c2
mustafa rehayee <mustafamostafa12@gmail.com>
2024-10-15 18:19:08 +0430

    1s commit

---
+++ set.py
@@ -0,0 +1,13 @@
+# Create a new set:
+print("Create a new set:")
+x = set()
+print(x)
+print(type(x))
+print("\nCreate a non-empty set:")
+n = set([0, 1, 2, 3, 4])
+print(n)
+print(type(n))
+print("\nUsing a literal:")
+a = {1, 2, 3, 'foo', 'bar'}
+print(type(a))
+print(a) 
---
+++ sumOfNumbersUsingList.py
@@ -0,0 +1,6 @@
+def sum(itmes):
+    result=0
+    for i in itmes:
+        result += i
+    return result
+print(sum([3, 3, 3]))
